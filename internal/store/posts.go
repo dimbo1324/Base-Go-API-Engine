13a997724aac0386bb1712fdfde3dc2adf6b91ae
dimbo1324 <dim4dmi7rij@yandex.ru>
2025-04-08 10:56:05 +0300

    Смотри прошлый коммит

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -8,13 +8,13 @@ import (
 )
 
 type Post struct {
-	Id        int64    `json: "id"`
-	UserId    int64    `json: "user_id"`
-	Content   string   `json: "content"`
-	Title     string   `json: "title"`
-	CreatedAt string   `json: "created_at"`
-	UpdatedAt string   `json: "updated_at"`
-	Tags      []string `json: "tags"`
+	Id        int64    `json:"id"`
+	UserId    int64    `json:"user_id"`
+	Content   string   `json:"content"`
+	Title     string   `json:"title"`
+	CreatedAt string   `json:"created_at"`
+	UpdatedAt string   `json:"updated_at"`
+	Tags      []string `json:"tags"`
 }
 type PostStore struct {
 	db *sql.DB
