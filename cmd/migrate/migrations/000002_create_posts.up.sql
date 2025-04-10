a7c988717cf37e225b2c3fef9fe9fbbf3fd04beb
dimbo1324 <dim4dmi7rij@yandex.ru>
2025-04-10 22:01:18 +0300

    добавил sql скрипт для posts

--- cmd/migrate/migrations/000002_create_posts.up.sql
+++ cmd/migrate/migrations/000002_create_posts.up.sql
@@ -1,12 +1,10 @@
-CREATE EXTENSION IF NOT EXISTS citext;
-
 CREATE TABLE
-    IF NOT EXISTS users (
+    IF NOT EXISTS posts (
         id bigserial PRIMARY KEY,
-        email citext UNIQUE NOT NULL,
-        username varchar(255) UNIQUE NOT NULL,
-        password bytea NOT NULL,
+        title text NOT NULL,
+        user_id bigint NOT NULL,
+        content text NOT NULL,
         created_at timestamp(0)
-        with
-            time zone NOT NULL DEFAULT NOW ()
+        WITH
+            TIME ZONE NOT NULL DEFAULT NOW ()
     );
